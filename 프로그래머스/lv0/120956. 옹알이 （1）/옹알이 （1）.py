fc67b6f2d9a1a376f163799287cb3b9b1f180ebd
KIM DA MIN <122325788+meokku@users.noreply.github.com>
2023-01-13 02:38:47 +0900

    [level 0] Title: 옹알이 (1), Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.py
@@ -0,0 +1,12 @@
+from itertools import permutations
+def solution(babbling):
+    answer = 0
+    speek = ["aya","ye","woo","ma"]
+    word = []
+    for i in range(1,5):
+        for j in permutations(speek, i):
+            word.append(''.join(j))
+    for i in babbling:
+        if i in word:
+            answer += 1
+    return answer
