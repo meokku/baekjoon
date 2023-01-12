b7f37cc53da4cff6ab6dada922d82c4f9ad1c58b
KIM DA MIN <122325788+meokku@users.noreply.github.com>
2023-01-13 00:55:25 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 9052 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,9 @@
+from sys import stdin
+input=stdin.readline
+L=[0]*10000
+for _ in range(int(input())):
+    L[int(input())-1]+=1
+for i in range(10000):
+    if L[i]!=0:
+        for j in range(L[i]):
+            print(i+1)
