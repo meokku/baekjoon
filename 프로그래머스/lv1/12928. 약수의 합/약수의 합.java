52b2d10f30ee9ce78aeb711065a73b462ec71da8
KIM DA MIN <122325788+meokku@users.noreply.github.com>
2023-01-24 16:13:11 +0900

    [level 1] Title: 약수의 합, Time: 0.06 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.java
@@ -1,11 +1,9 @@
 class Solution {
     public int solution(int n) {
         int R=0;
-        for(int i=1;i<=n;i++){
-            if(n%i==0){
-                R+=i;
-            }
+        for(int i=1;i<=n/2;i++){
+            if(n%i==0) R+=i;
         }
-        return R;
+        return R+n;
     }
 }
