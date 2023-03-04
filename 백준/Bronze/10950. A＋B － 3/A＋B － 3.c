81f371ffa3f18b2c09ff969938cf2b8470eab519
KIM DA MIN <122325788+meokku@users.noreply.github.com>
2023-03-05 00:41:09 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int T,a,b;
+    int* pa=&a;
+    int* pb=&b;
+    
+    scanf("%d",&T);
+    for(int i=0;i<T;i++){
+        scanf("%d %d",pa,pb);
+        printf("%d\n",*pa+*pb);
+    }
+    return 0;
+}
