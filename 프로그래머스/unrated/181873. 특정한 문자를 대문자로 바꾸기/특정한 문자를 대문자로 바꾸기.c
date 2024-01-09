1be7546bc05c6caad186a4b5f0815e1556a4b6d2
KIM DA MIN <122325788+meokku@users.noreply.github.com>
2024-01-09 21:45:03 +0900

    [level unrated] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* my_string, const char* alp) {
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    char* answer = (char*)malloc(strlen(my_string)*sizeof(char*));
+    strcpy(answer,my_string);
+    char* ptr=strstr(answer,alp);
+    while(ptr!=NULL){
+        ptr[0]-=32;
+        ptr=strstr(ptr+1,alp);
+    }
+    return answer;
+}
