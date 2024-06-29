f336b8aad95445af635fb77058b67424d1c3d642
KIM DA MIN <122325788+meokku@users.noreply.github.com>
2024-06-29 21:44:47 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.c
+++ 백준/Bronze/1008. A／B/A／B.c
@@ -1,3 +1,8 @@
 #include <stdio.h>
-int main()
-{int a,b;scanf("%d %d",&a,&b);printf("%.9f",(double)a/b);return 0;}
+
+int main(){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    printf("%.9f",(double)a/b);
+    return 0;
+}
